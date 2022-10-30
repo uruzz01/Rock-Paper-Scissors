dd3768df2ed7521f76b2c6a3482fc4fef25a4f2c
uvarov <uvarov.1337@gmail.com>
2022-10-30 16:17:30 +0200

    changed and finished a getPlayerChoice function at script.js

--- script.js
+++ script.js
@@ -11,22 +11,9 @@ function getPlayerChoice()
 {
     userItem = userItem.toLowerCase();
 
-    console.log(userItem);
-    
-    // if (userItem !== 'rock' && userItem !== 'paper' && userItem !== 'scissors')
-    // {
-    //     console.log("invalid item. item should be: 'rock', 'paper', 'scissors'")
-    //     return;
-    // }
-
     let result = userItem === 'rock' ? 'rock' :
         userItem === 'paper' ? 'paper' :
         userItem === 'scissors' ? 'scissors' : "invalid item. item should be: 'rock', 'paper', 'scissors'";
 
-    console.log(result);
-
     return result;
 }
-
-getPlayerChoice();
-
