bd360fb8d8715c0a366b86c18d34931d78f95f14
uvarov <uvarov.1337@gmail.com>
2022-10-30 16:53:08 +0200

    made a playRound function at script.js

--- script.js
+++ script.js
@@ -17,3 +17,47 @@ function getPlayerChoice()
 
     return result;
 }
+
+function playRound(playerSelection, computerSelection) 
+{
+    // playerSelection = getPlayerChoice();
+    // computerSelection = getComputerChoice();
+
+    console.log(`Player: ${playerSelection}`);
+    console.log(`Computer: ${computerSelection}`);
+
+    if (playerSelection === computerSelection) 
+    {
+        return "It's a tie!";
+    } 
+    else if (playerSelection === 'rock' && computerSelection === 'scissors')
+    {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'paper')
+    {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'rock')
+    {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (playerSelection === 'scissors' && computerSelection === 'rock')
+    {
+        return `You lost! ${computerSelection} beats ${playerSelection}`;
+    }
+    else if (playerSelection === 'paper' && computerSelection === 'scissors')
+    {
+        return `You lost! ${computerSelection} beats ${playerSelection}`;
+    }
+    else if (playerSelection === 'rock' && computerSelection === 'paper')
+    {
+        return `You lost! ${computerSelection} beats ${playerSelection}`;
+    } 
+    else 
+    {
+        return "invalid item. item should be: 'rock', 'paper', 'scissors'";
+    }
+}
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
