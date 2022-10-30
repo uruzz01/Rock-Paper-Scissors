06e7862dd8c61fd6df739d26798b0ff38d10a410
uvarov <uvarov.1337@gmail.com>
2022-10-30 16:01:34 +0200

    made a getPlayerChoice at script.js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-let itemsArr = ['rock', 'paper', "scissors"];
+let itemsArr = ['rock', 'paper', 'scissors'];
 let userItem = prompt('Choose between: rock, paper, scissors', '');
  
-function getComputerChoie()
+function getComputerChoice()
 {
   return itemsArr[Math.floor(Math.random()*itemsArr.length)];
 }
+
+function getPlayerChoice()
+{
+    userItem.toLowerCase();
+    
+    if (userItem !== 'rock' && userItem !== 'paper' && userItem !== 'scissors')
+    {
+        console.log("invalid item. item should be: 'rock', 'paper', 'scissors'")
+        return;
+    }
+}
+
