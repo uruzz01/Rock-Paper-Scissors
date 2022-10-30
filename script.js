fce04dad89378c70e2068f5f947849e28a917a63
uvarov <uvarov.1337@gmail.com>
2022-10-30 19:00:13 +0200

    made a made a final game function at script.js. Need to fix an error if prompt is empty or closed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let itemsArr = ['rock', 'paper', 'scissors'];
-let userItem = prompt('Choose between: rock, paper, scissors', '');
+
  
 function getComputerChoice()
 {
@@ -7,57 +7,119 @@ function getComputerChoice()
 }
 
 
-function getPlayerChoice()
+function getPlayerChoice(item)
 {
-    userItem = userItem.toLowerCase();
+    item = item.toLowerCase();
 
-    let result = userItem === 'rock' ? 'rock' :
-        userItem === 'paper' ? 'paper' :
-        userItem === 'scissors' ? 'scissors' : "invalid item. item should be: 'rock', 'paper', 'scissors'";
+    let result = item === 'rock' ? 'rock' :
+        item === 'paper' ? 'paper' :
+        item === 'scissors' ? 'scissors' : "invalid item. item should be: 'rock', 'paper', 'scissors'";
 
     return result;
 }
 
 function playRound(playerSelection, computerSelection) 
 {
+
+    // console.log(`Player: ${playerSelection}`);
+    // console.log(`Computer: ${computerSelection}`);
+
     // playerSelection = getPlayerChoice();
     // computerSelection = getComputerChoice();
 
-    console.log(`Player: ${playerSelection}`);
-    console.log(`Computer: ${computerSelection}`);
-
     if (playerSelection === computerSelection) 
     {
-        return "It's a tie!";
+        console.log("It's a tie!");
+        return
     } 
     else if (playerSelection === 'rock' && computerSelection === 'scissors')
     {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        return 'win';
     }
     else if (playerSelection === 'scissors' && computerSelection === 'paper')
     {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        return 'win';
     }
     else if (playerSelection === 'paper' && computerSelection === 'rock')
     {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        return 'win';
     }
     else if (playerSelection === 'scissors' && computerSelection === 'rock')
     {
-        return `You lost! ${computerSelection} beats ${playerSelection}`;
+        console.log(`You lost! ${computerSelection} beats ${playerSelection}`);
+        return 'lose';
     }
     else if (playerSelection === 'paper' && computerSelection === 'scissors')
     {
-        return `You lost! ${computerSelection} beats ${playerSelection}`;
+        console.log(`You lost! ${computerSelection} beats ${playerSelection}`);
+        return 'lose';
     }
     else if (playerSelection === 'rock' && computerSelection === 'paper')
     {
-        return `You lost! ${computerSelection} beats ${playerSelection}`;
+        console.log(`You lost! ${computerSelection} beats ${playerSelection}`);
+        return 'lose';
     } 
     else 
     {
-        return "invalid item. item should be: 'rock', 'paper', 'scissors'";
+        console.log("invalid item. item should be: 'rock', 'paper', 'scissors'");
+        return 
     }
 }
+
+function game()
+{
+    let gamesRound = 0;
+    let playerWins = 0;
+    let computerWins = 0;
+    let roundResult;
+  
+
+    for (;;)
+    {
+
+        // console.log(roundResult);
+
+        if (playerWins === 5)
+        {
+            console.log('Player won!');
+            break;
+        }
+
+        if(computerWins === 5)
+        {
+            console.log('Computer won!');
+            break;
+        }
+
+        let userItem = prompt('Choose between: rock, paper, scissors', '');
+
+        roundResult = playRound(getPlayerChoice(userItem), getComputerChoice());
+
+        if (roundResult === 'win') 
+        {
+            playerWins += 1;
+            gamesRound += 1;
+            console.log(`Player wins: ${playerWins}`);
+        } 
+        else if (roundResult === 'lose')
+        {
+            computerWins += 1;
+            gamesRound += 1;
+            console.log(`Computer wins: ${computerWins}`);
+        } 
+        else
+        {
+            gamesRound += 1;
+        }
+    } 
+
+    console.log(`Total rounds: ${gamesRound}`);
+
+}
+
+game();
 
 // console.log(playRound(getPlayerChoice(), getComputerChoice()));
